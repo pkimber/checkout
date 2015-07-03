5efe6b54179b1d63428b29d027561a4d7d4f7f1c
Patrick Kimber <code@pkimber.net>
2015-07-03 19:31:20 +0100

    wip - process payment plans #644

--- example_checkout/tests/test_contact_plan_payment.py
+++ example_checkout/tests/test_contact_plan_payment.py
@@ -5,7 +5,10 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 from decimal import Decimal
 
-from checkout.models import ContactPlanPayment
+from checkout.models import (
+    CheckoutState,
+    ContactPlanPayment,
+)
 from .factories import (
     ContactPlanFactory,
     ContactPlanPaymentFactory,
@@ -28,7 +31,7 @@ def test_due():
 
 
 @pytest.mark.django_db
-def test_due_not_deleted():
+def test_due_plan_deleted():
     today = date.today()
     ContactPlanPaymentFactory(
         due=today+relativedelta(days=1),
@@ -38,6 +41,45 @@ def test_due_not_deleted():
     ContactPlanPaymentFactory(
         contact_plan=contact_plan,
         due=today+relativedelta(days=2),
+        amount=Decimal('2')
+    )
+    ContactPlanPaymentFactory(
+        due=today+relativedelta(days=3),
+        amount=Decimal('3')
+    )
+    result = [p.amount for p in ContactPlanPayment.objects.due]
+    assert [Decimal('1'), Decimal('3')] == result
+
+
+@pytest.mark.django_db
+def test_due_not_due():
+    today = date.today()
+    ContactPlanPaymentFactory(
+        due=today+relativedelta(days=1),
+        amount=Decimal('1')
+    )
+    ContactPlanPaymentFactory(
+        due=today+relativedelta(days=-1),
+        amount=Decimal('2')
+    )
+    ContactPlanPaymentFactory(
+        due=today+relativedelta(days=3),
+        amount=Decimal('3')
+    )
+    result = [p.amount for p in ContactPlanPayment.objects.due]
+    assert [Decimal('1'), Decimal('3')] == result
+
+
+@pytest.mark.django_db
+def test_due_not_pending():
+    today = date.today()
+    ContactPlanPaymentFactory(
+        due=today+relativedelta(days=1),
+        amount=Decimal('1')
+    )
+    ContactPlanPaymentFactory(
+        due=today+relativedelta(days=2),
+        state=CheckoutState.objects.fail,
         amount=Decimal('2')
     )
     ContactPlanPaymentFactory(
