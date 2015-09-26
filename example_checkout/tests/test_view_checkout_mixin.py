018f39b9e6d6e2cb9e8f23b4b597945d5643619c
Patrick Kimber <code@pkimber.net>
2015-09-26 09:58:54 +0100

    email notifications - admin #1089

--- example_checkout/tests/test_view_checkout_mixin.py
+++ example_checkout/tests/test_view_checkout_mixin.py
@@ -11,6 +11,8 @@ from checkout.models import (
 from checkout.tests.factories import CheckoutSettingsFactory
 from checkout.views import CONTENT_OBJECT_PK
 from example_checkout.tests.factories import SalesLedgerFactory
+from mail.models import Message
+from mail.tests.factories import NotifyFactory
 from stock.tests.factories import ProductFactory
 
 
@@ -34,6 +36,7 @@ def test_get(client):
 def test_post_card_payment(client, mocker):
     mocker.patch('stripe.Charge.create')
     mocker.patch('stripe.Customer.create')
+    NotifyFactory()
     obj = SalesLedgerFactory()
     _set_session(client, obj.pk)
     url = reverse('example.sales.ledger.checkout', args=[obj.pk])
@@ -47,11 +50,17 @@ def test_post_card_payment(client, mocker):
     checkout = Checkout.objects.first()
     assert CheckoutAction.PAYMENT == checkout.action.slug
     assert Decimal('0') < checkout.total
+    # check email notification
+    assert 1 == Message.objects.count()
+    message = Message.objects.first()
+    assert 'SUCCESS - Payment' in message.subject
+    assert 'SUCCESS - Payment from' in message.description
 
 
 @pytest.mark.django_db
 def test_post_card_payment_plan(client, mocker):
     mocker.patch('stripe.Customer.create')
+    NotifyFactory()
     product = ProductFactory(price=Decimal('12.34'))
     obj = SalesLedgerFactory(product=product)
     _set_session(client, obj.pk)
@@ -66,11 +75,17 @@ def test_post_card_payment_plan(client, mocker):
     checkout = Checkout.objects.first()
     assert CheckoutAction.PAYMENT_PLAN == checkout.action.slug
     assert Decimal('12.34') == checkout.total
+    # check email notification
+    assert 1 == Message.objects.count()
+    message = Message.objects.first()
+    assert 'SUCCESS - Payment plan' in message.subject
+    assert 'SUCCESS - Payment Plan from' in message.description
 
 
 @pytest.mark.django_db
 def test_post_card_refresh(client, mocker):
     mocker.patch('stripe.Customer.create')
+    NotifyFactory()
     obj = SalesLedgerFactory()
     _set_session(client, obj.pk)
     url = reverse('example.sales.ledger.checkout', args=[obj.pk])
@@ -84,10 +99,16 @@ def test_post_card_refresh(client, mocker):
     checkout = Checkout.objects.first()
     assert CheckoutAction.CARD_REFRESH == checkout.action.slug
     assert None == checkout.total
+    # check email notification
+    assert 1 == Message.objects.count()
+    message = Message.objects.first()
+    assert 'SUCCESS - Card refresh' in message.subject
+    assert 'SUCCESS - Card Refresh from' in message.description
 
 
 @pytest.mark.django_db
 def test_post_invoice(client):
+    NotifyFactory()
     obj = SalesLedgerFactory()
     _set_session(client, obj.pk)
     url = reverse('example.sales.ledger.checkout', args=[obj.pk])
@@ -110,3 +131,8 @@ def test_post_invoice(client):
     invoice = checkout.checkoutinvoice
     assert 'test@test.com' == invoice.email
     assert Decimal('0') < checkout.total
+    # check email notification
+    assert 1 == Message.objects.count()
+    message = Message.objects.first()
+    assert 'SUCCESS - Invoice' in message.subject
+    assert 'SUCCESS - Invoice' in message.description
